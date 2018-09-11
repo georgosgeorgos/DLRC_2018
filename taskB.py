9e18f5d711985928d2c488140a477e8101608546
dlrc <vwdlrc2@gmail.com>
2018-09-11 11:00:24 +0200

    define variables

--- taskB.py
+++ taskB.py
@@ -12,19 +12,22 @@ state = b.recv_msg("franka_state", -1)
 current_pos_c = state.get_c_pos()
 print('current pos: {}'.format(current_pos_c))
 
+n_points = 10
+n_runs = 3
+time2go = 1.
+delta = 2 * np.pi / n_points
+R = 0.2
 
-delta = 2 * np.pi / 10
+for theta in np.arange(0, 2*n_runs*np.pi, delta):
 
-for theta in np.arange(0, 6*np.pi, delta):
-
-    pos_c = current_pos_c + [0.2*np.sin(theta), 0.2*np.cos(theta), 0]
+    pos_c = current_pos_c + [R*np.sin(theta), R*np.cos(theta), 0]
 
     msg = pab.target_pos_msg()
-    msg.set_ctrl_t(0)
+    msg.set_ctrl_t(pab.CtrlType.Cartesian)
     msg.set_pos(pos_c)
     msg.set_timestamp(time.clock_gettime(time.CLOCK_MONOTONIC))
     msg.set_fnumber(counter)
-    msg.set_time_to_go(1.)
+    msg.set_time_to_go(time2go)
 
     b.send_msg("franka_target_pos", msg)
     time.sleep(0.5)
