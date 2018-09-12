a59980012f77b2bcfbc4fbf765529e937a6005b0
dlrc <vwdlrc2@gmail.com>
2018-09-12 15:44:49 +0200

    data 10runs

---
+++ lidar_conf.py
@@ -0,0 +1,33 @@
+import py_at_broker as pab
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+b = pab.broker()
+print(b.request_signal("franka_lidar", pab.MsgType.franka_lidar))
+
+counter = 0
+
+start = time.time()
+lidar_list = []
+
+while True:
+
+    try:
+        lidar = b.recv_msg("franka_lidar", -1)
+
+        lidar_list.append(lidar.get_data())
+        counter += 1
+    except KeyboardInterrupt:
+        break
+
+    print(counter)
+
+
+lidar_array = np.array(lidar_list)
+
+for j in range(9):
+    plt.plot(lidar_array[:,j])
+    plt.title(j+1)
+    plt.show()
--- main.py
+++ main.py
@@ -3,27 +3,19 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 import seaborn as sns
+from PIL import Image
+from libtiff import TIFF
 
 b = pab.broker()
 print(b.register_signal("franka_target_pos", pab.MsgType.target_pos))
 time.sleep(0.5)
 print(b.request_signal("franka_lidar", pab.MsgType.franka_lidar))
+print(b.request_signal("realsense_images", pab.MsgType.realsense_image))
 print(b.request_signal("franka_state", pab.MsgType.franka_state))
 time.sleep(0.5)
 
 counter = 0
 default_pos_c = np.array([0.62, 0.00, 0.56])
-
-# def default_pos():
-#     pos_c = np.array([ 6.23963393e-01, -5.11126213e-06,  5.59791033e-01])
-#     msg = pab.target_pos_msg()
-#     msg.set_ctrl_t(pab.CtrlType.Cartesian)
-#     msg.set_pos(pos_c)
-#     msg.set_timestamp(time.clock_gettime(time.CLOCK_MONOTONIC))
-#     msg.set_fnumber(counter)
-#     msg.set_time_to_go(2.)
-#     b.send_msg("franka_target_pos", msg)
-#     return None
 
 def f_msg(pos):
     msg = pab.target_pos_msg()
@@ -34,21 +26,20 @@ def f_msg(pos):
     msg.set_time_to_go(time2go)
     b.send_msg("franka_target_pos", msg)
 
-
 state = b.recv_msg("franka_state", -1)    
 current_pos_c = state.get_c_pos()
 print('initial pos: {}'.format(current_pos_c))
 
 n_points = 10
-n_runs = 2
+n_runs = 20
 time2go = 1.
 delta = 2 * np.pi / n_points
 R = 0.2
 
 lidar_list = []
 c = 0
-n_samples=100
-
+n_samples=50
+img_counter = 0
 while True:
     f_msg(default_pos_c)
     time.sleep(0.5)
@@ -58,11 +49,10 @@ while True:
     print('current pos: {}'.format(current_pos_c))
     counter += 1
     time.sleep(2.0)
-    for n in range(1, n_runs+1):
+    for n in range(n_runs):
         print("RUN:", n)
         print()
         for theta in np.arange(0, 2*np.pi, delta):
-
             # define next position
             pos_c = default_pos_c + [R*np.sin(theta), R*np.cos(theta), 0]
 
@@ -80,20 +70,19 @@ while True:
                 counter += 1
                 c += 1
 
-            #lidar_array = np.array(lidar_list)
             print("end lidar data collected")
-            #time.sleep(0.5)
-            #lidar_array[lidar_array > 1900] = 0
-            #print(lidar_array.shape)
+            img = b.recv_msg("realsense_images", -1)
 
-            # print("plot hist")
-            # for j in range(9):
-            #     plt.plot(lidar_array[:,j])
-            #     plt.show()
-            #     plt.hist(lidar_array[:,j][lidar_array[:,j] > 0], bins=30, density=True)
-            #     plt.show()
+            rgb   = np.reshape(img.get_rgb(), img.get_shape_rgb())
+            rgb = Image.fromarray(rgb)
+            rgb.save('./images/rgb/' + str(img_counter) + '.png')
 
-            # plt.show()
+            depth = np.reshape(img.get_depth(), img.get_shape_depth())
+            tiff = TIFF.open('./images/depth/' + str(img_counter) + '.tiff', mode='w')
+            tiff.write_image(depth)
+            tiff.close()
+
+            img_counter += 1
 
             new_state = b.recv_msg("franka_state", -1)
             print('next pos: {}'.format(new_state.get_c_pos()))
@@ -101,7 +90,7 @@ while True:
 
     lidar_array = np.array(lidar_list)
     print(lidar_array.shape)
-    np.save("lidar_data.npy", lidar_array)
+    np.save("lidar_data_10runs.npy", lidar_array)
     break
 
 
