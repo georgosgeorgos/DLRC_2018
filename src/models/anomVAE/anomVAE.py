8a097ebe793633c5a7965b35b9e06d0dac023314
dlrc <vwdlrc2@gmail.com>
2018-09-27 15:52:23 +0200

    add joint data to VAEanom

--- src/models/anomVAE/anomVAE.py
+++ src/models/anomVAE/anomVAE.py
@@ -18,11 +18,11 @@ import numpy as np
 parser = argparse.ArgumentParser(description='VAE Anomaly Example')
 parser.add_argument('--batch_size', type=int, default=32, metavar='N',
                     help='input batch size for training (default: 32)')
-parser.add_argument('--code_size', type=int, default=3, metavar='N',
+parser.add_argument('--code_size', type=int, default=10, metavar='N',
                     help='code size (default: 2)')
 parser.add_argument('--lr', type=float, default=1e-4, metavar='N',
                     help='learning rate (default: 1e-3)')
-parser.add_argument('--epochs', type=int, default=1000, metavar='N',
+parser.add_argument('--epochs', type=int, default=300, metavar='N',
                     help='number of epochs to train (default: 150)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
@@ -39,7 +39,7 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 test_every_nth = 1
 n_joints = 7
 n_lidars = 9
-n_hidden = 64
+n_hidden = 128
 verbose = False
 trbs = 512
 tebs = 256
@@ -71,11 +71,11 @@ class VAE(nn.Module):
     def __init__(self):
         super(VAE, self).__init__()
 
-        self.fc1 = nn.Linear(n_lidars, n_hidden)
+        self.fc1 = nn.Linear(n_lidars+2*n_joints, n_hidden)
         self.fc21 = nn.Linear(n_hidden, args.code_size)
         self.fc22 = nn.Linear(n_hidden, args.code_size)
         self.fc3 = nn.Linear(args.code_size, n_hidden)
-        self.fc4 = nn.Linear(n_hidden, n_lidars)
+        self.fc4 = nn.Linear(n_hidden, n_lidars+2*n_joints)
 
     def encode(self, *input):
         h1 = th.tanh(self.fc1(input[0]))
@@ -94,7 +94,7 @@ class VAE(nn.Module):
         return th.tanh(self.fc4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, n_lidars))
+        mu, logvar = self.encode(x.view(-1, n_lidars+2*n_joints))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
@@ -102,9 +102,10 @@ class VAE(nn.Module):
 model = VAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
+
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
-    BCE = F.mse_loss(recon_x, x.view(-1, n_lidars), reduction='sum')
+    BCE = F.mse_loss(recon_x, x.view(-1, n_lidars+2*n_joints), reduction='sum')
 
     # see Appendix B from Generative_Models paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
@@ -119,19 +120,24 @@ def train(epoch):
     model.train()
     train_loss = 0.
     for batch_idx, (x, y, z) in enumerate(train_loader):
-        y = y.to(device)
+        x = x.to(device).float()
+        y = y.to(device).float()
+        z = z.to(device).float()
+
+        input_concat = th.cat((x, y, z), dim=1)
+
         optimizer.zero_grad()
-        recon_batch, mu, logvar = model(y)
-        loss = loss_function(recon_batch, y, mu, logvar)
+        recon_batch, mu, logvar = model(input_concat)
+        loss = loss_function(recon_batch, input_concat, mu, logvar)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
         if verbose:
             if batch_idx % args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(y), len(train_loader.dataset),
+                    epoch, batch_idx * len(input_concat), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader),
-                    loss.item() / len(y)))
+                    loss.item() / len(input_concat)))
     epoch_loss = train_loss / len(train_loader.dataset)
     print('train epoch: {} avg. loss: {:.4f}'.format(epoch, epoch_loss))
 
@@ -147,11 +153,17 @@ def test(epoch):
     with th.no_grad():
         for i, (x, y, z) in enumerate(test_loader):
             bs, _, = y.size()
-            y = y.to(device)
-            y_recon, mu, logvar = model(y)
-            test_loss += loss_function(y_recon, y, mu, logvar).item()
 
-            MSE += F.mse_loss(y, y_recon, reduction='elementwise_mean')
+            x = x.to(device).float()
+            y = y.to(device).float()
+            z = z.to(device).float()
+
+            input_concat = th.cat((x, y, z), dim=1)
+
+            input_recon, mu, logvar = model(input_concat)
+            test_loss += loss_function(input_recon, input_concat, mu, logvar).item()
+
+            MSE += F.mse_loss(input_concat, input_recon, reduction='elementwise_mean')
 
     epoch_loss = test_loss / len(test_loader.dataset)
     epoch_RMSE = th.sqrt(MSE / len(test_loader.dataset))
@@ -175,9 +187,9 @@ if __name__ == '__main__':
             test_loss_history.append(epoch_loss)
             test_RMSE_history.append(epoch_RMSE)
 
-        plot_eval(np.arange(len(train_loss_history)), np.array(train_loss_history), xlabel='epochs', ylabel='loss',
-                  title='train loss', save_to=osp.join(path_results, 'train_loss.png'))
-        plot_eval(np.arange(len(test_loss_history)), np.array(test_loss_history), xlabel='epochs', ylabel='loss',
-                  title='test loss', save_to=osp.join(path_results, 'test_loss.png'))
-        plot_eval(np.arange(len(test_RMSE_history)), np.array(test_RMSE_history), xlabel='epochs', ylabel='RMSE',
-                  title='test RMSE', save_to=osp.join(path_results, 'test_RMSE.png'))
+    plot_eval(np.arange(len(train_loss_history)), np.array(train_loss_history), xlabel='epochs', ylabel='loss',
+              title='train loss', save_to=osp.join(path_results, 'train_loss.png'))
+    plot_eval(np.arange(len(test_loss_history)), np.array(test_loss_history), xlabel='epochs', ylabel='loss',
+              title='test loss', save_to=osp.join(path_results, 'test_loss.png'))
+    plot_eval(np.arange(len(test_RMSE_history)), np.array(test_RMSE_history), xlabel='epochs', ylabel='RMSE',
+              title='test RMSE', save_to=osp.join(path_results, 'test_RMSE.png'))
