dc1ca64588e46c1a8b34c9515423642383f5fe03
dlrc <vwdlrc2@gmail.com>
2018-10-05 10:27:55 +0200

    update vis

--- visualization/app.py
+++ visualization/app.py
@@ -7,6 +7,7 @@ from plotly import tools
 from dash.dependencies import Input, Output
 from plotly.graph_objs import *
 import numpy as np
+from scipy.special import expit, logit
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -15,7 +16,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(
     html.Div([
         html.H4('Lidars visualizations'),
-        dcc.Graph(id='live-update-graph'),
+        dcc.Graph(id='live-update-graph-lidars'),
+        dcc.Graph(id='live-update-graph-probs'),
         dcc.Interval(
             id='interval-component',
             interval=0.5 * 1000,  # one 1/4 of a second
@@ -25,17 +27,18 @@ app.layout = html.Div(
 )
 
 
-@app.callback(Output('live-update-graph', 'figure'),
+@app.callback(Output('live-update-graph-lidars', 'figure'),
               [Input('interval-component', 'n_intervals')])
-def update_graph_live(n_intervals):
+def update_graph_lidars(n_intervals):
 
     # demo data
-    lidar_data = np.random.randn(100)
+    lidar_data = np.random.random(100)
 
     trace = go.Scatter(
         x=np.arange(len(lidar_data)) + len(lidar_data) * n_intervals,
         y=lidar_data,
-        mode='lines+markers'
+        mode='lines+markers',
+        fillcolor='#000000'
     )
 
     layout = Layout(
@@ -44,11 +47,52 @@ def update_graph_live(n_intervals):
             type='log'),
         yaxis=dict(
             title='Depth (m)',
-            range=[-3, 3]
+            range=[0, 2]
         )
     )
 
     return Figure(data=[trace], layout=layout)
+
+
+@app.callback(Output('live-update-graph-probs', 'figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_graph_probs(n_intervals):
+
+    # demo data
+    probs_normal = np.ones(100)
+
+    trace = go.Scatter(
+        x=np.arange(len(probs_normal)) + len(probs_normal) * n_intervals,
+        y=probs_normal,
+        mode='lines+markers',
+        fill='tozeroy',
+        fillcolor='#cce6ff'
+    )
+
+    # demo data
+    probs_anom = expit(np.random.randn(100))
+
+    trace_b = go.Scatter(
+        x=np.arange(len(probs_anom)) + len(probs_anom) * n_intervals,
+        y=probs_anom,
+        mode='lines+markers',
+        fill='tozeroy',
+        fillcolor='#ffcccc',
+        line=dict(color='#b30000', smoothing=1.3)
+    )
+
+    layout = Layout(
+        xaxis=dict(
+            title='Time',
+            type='log'),
+        yaxis=dict(
+            title='Prob',
+            range=[0, 1]
+        ),
+        height=250
+    )
+
+    return Figure(data=[trace, trace_b], layout=layout)
 
 
 if __name__ == '__main__':
