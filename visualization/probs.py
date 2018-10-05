c35169c0d9cb6fe3f9aca83fa73f6de965ab0000
dlrc <vwdlrc2@gmail.com>
2018-10-05 13:09:04 +0200

    update sampler and probs demo

--- visualization/probs.py
+++ visualization/probs.py
@@ -6,58 +6,48 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 from torch.utils.data import DataLoader
 import torch as th
-from load_panda import PandaDataSet
+from sampler import Sampler
 from normalMLP import NormalMLP
 import os.path as osp
 from torch.distributions.normal import Normal
 
-class Sampler:
+class Probs:
     def __init__(self, n=100, l=3):
-        cuda = th.cuda.is_available()
-        self.device = th.device("cuda" if cuda else "cpu")
-        kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
-
-        test_set = PandaDataSet(root_dir="../data/", filename='anomaly_detection_gt.pkl', train=False, test_split=0.0, transform=transforms.Compose([
-            transforms.Lambda(lambda n: th.Tensor(n)),
-            transforms.Lambda(lambda n: th.Tensor.clamp(n, 14, 2000)),
-            transforms.Lambda(lambda n: n / 1000)
-        ]))
-        self.test_loader = DataLoader(test_set, batch_size=n, shuffle=False, **kwargs)
-
+        self.cuda = th.cuda.is_available()
+        self.device = th.device("cuda" if self.cuda else "cpu")
         self.model = NormalMLP().to(self.device)
-
-        saved_state_dict = th.load("../experiments/normalMLP/ckpt/ckpt.pkl")[0]
-        self.model.load_state_dict(saved_state_dict)
+        self.model.load_state_dict(th.load("../experiments/normalMLP/ckpt/ckpt.pkl"))
         self.model.eval()
 
         self.index=0
+        self.n = n
+        self.l = l
+        self.sampler = Sampler(self.n)
 
-    def routine_index(self):
-        if self.index >= (len(self.test_loader)-1):
-            self.index = 0
-
-    def get_sample(self):
-        self.routine_index()
+    def get_data(self):
+        y, x, _ = self.sampler.get_sample()
+        print(x.shape, y.shape)
 
         data = {}
-        (x, y, _) = test_loader[self.index]     
-        x = x.to(self.device).float()
-        y = y.to(self.device).float()
+        x = th.from_numpy(x).to(self.device).float()
+        y = th.from_numpy(y).to(self.device).float()
 
         mu, logvar = self.model(x)
         std = th.exp(0.5 * logvar)
         N = Normal(mu, std)
-        anomaly_prob = N.log_prob(y)
+        prob = N.log_prob(y)
 
-        y            = y.cpu().data.numpy().tolist()
-        mu           = mu.cpu().data.numpy().tolist()
-        std          = std.cpu().data.numpy().tolist()
-        pred         = y_sample.cpu().data.numpy().tolist()
-        anomaly_prob = anomaly_prob.cpu().data.numpy().tolist()
+        y            = y.cpu().data.numpy()
+        mu           = mu.cpu().data.numpy()
+        std          = std.cpu().data.numpy()
+        prob         = prob.cpu().data.numpy()
 
-        data =  {"input": y, "pred": pred, "mu": mu, "std": std, "anomaly_prob": anomaly_prob}
+        data =  {"input": y, "mu": mu, "std": std, "prob": prob}
         return data
 
 if __name__ == '__main__':
-    s = Sampler()
-    print(s.get_sample())
+    p =Probs()
+    d =p.get_data()
+    for i in d.keys():
+        print(i, d[i][:10,3])
+        print()
--- visualization/sampler.py
+++ visualization/sampler.py
@@ -3,51 +3,43 @@ import numpy as np
 
 
 class Sampler:
-    def __init__(self):
-        with open("../data/train_data_correct.pkl", "rb") as f:
+    def __init__(self, n=100):
+        with open("../data/anomaly_detection_gt.pkl", "rb") as f:
             self.data = pkl.load(f)
 
             self.runs = len(self.data)
-            self.data_lidar = []
+            self.data_lidar   = []
+            self.data_joint   = []
+            self.data_joint_v = []
             for i in range(self.runs):       
                 self.data_lidar.extend(self.data[i]["lidar"]["measurements"])
-
+                self.data_joint.extend(self.data[i]["state"]["j_pos"])
+                self.data_joint_v.extend(self.data[i]["state"]["j_vel"])
+            
             self.data_lidar = np.array(self.data_lidar, dtype=float)
             self.data_lidar /= 1000
             self.data_lidar[self.data_lidar > 2.0] = 2.0
 
+            self.data_joint = np.array(self.data_joint, dtype=float)
+            self.data_joint_v = np.array(self.data_joint_v, dtype=float)
+
         self.index=0
+        self.n = 100
 
-    def get_sample(self, n=100):
-        if self.index == (self.data_lidar.shape[0] - n):
+    def get_sample(self):
+        if self.index == (self.data_lidar.shape[0] - self.n):
             self.index = 0
-        sample = self.data_lidar[(self.index):(self.index+n)]
-        self.index += n
-        return sample
-
-    def get_sample_lidar(self, n=100, l=3):
-        if self.index == (self.data_lidar.shape[0] - n):
-            self.index = 0
-        sample = self.data_lidar[(self.index):(self.index+n), l]
-        print(self.index, (self.index+n))
-        self.index += n
-        return sample
-
-    def get_dataset(self):
-        return self.data_lidar
-
+        sample_lidar   = self.data_lidar[(self.index):(self.index+self.n)]
+        sample_joint   = self.data_joint[(self.index):(self.index+self.n)]
+        sample_joint_v = self.data_joint_v[(self.index):(self.index+self.n)]
+        self.index += self.n
+        return (sample_lidar, sample_joint, sample_joint_v)
 
 if __name__ == '__main__':
-    s = Sampler()
-    print(s.get_sample(100).shape)
-    print(s.get_sample_lidar(100).shape)
-    print(s.get_dataset().shape)
-
-    dataset = s.get_dataset()
     import matplotlib.pyplot as plt
     s = Sampler()
     for i in range(10):
-        dataset = s.get_sample_lidar(n=150, l=3)
-        plt.plot(dataset)
+        dataset, _, _ = s.get_sample()
+        plt.plot(dataset[:,3])
         plt.ylim([0,2])
         plt.show()
